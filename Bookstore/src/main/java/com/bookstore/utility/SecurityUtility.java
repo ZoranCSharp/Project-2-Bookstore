57699af022d15bce1b342cfb258e96daffa493c0
ZoranKJava <zorank.java@gmail.com>
2018-10-03 21:49:00 +0100

    Adding More to Login

--- Bookstore/src/main/java/com/bookstore/utility/SecurityUtility.java
+++ Bookstore/src/main/java/com/bookstore/utility/SecurityUtility.java
@@ -1,12 +1,37 @@
 package com.bookstore.utility;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import java.security.SecureRandom;
+import java.util.Random;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
 public class SecurityUtility {
 
+	private static final String SALT = "salt"; //Salt should be protected caefully
+	
+	@Bean
 	public static BCryptPasswordEncoder passwordEncoder() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return new BCryptPasswordEncoder(12, new SecureRandom(SALT.getBytes()));
+	}
+	
+	@Bean
+	public static String randomPassword() {
+		
+		String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVQXYZ123456789";
+		StringBuilder salt = new StringBuilder();
+		Random rnd = new Random();
+		
+		while(salt.length()<18) {
+			int index = (int) (rnd.nextFloat()*SALTCHARS.length());
+			salt.append(SALTCHARS.charAt(index));
+		}
+		
+		String saltStr = salt.toString();
+		return saltStr;
 	}
 
 }
