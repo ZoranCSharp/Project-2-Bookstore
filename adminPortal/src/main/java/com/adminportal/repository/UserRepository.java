73e552260a1e2abe318a00dd902b5704cd136520
ZoranKJava <zorank.java@gmail.com>
2018-10-08 19:48:23 +0100

    Admin Portal - Login Page

---
+++ adminPortal/src/main/java/com/adminportal/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.adminportal.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.adminportal.domain.security.UserRole;
+import com.adminportal.domain.User;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+	
+	User findByUsername(String username);
+	
+	
+}
