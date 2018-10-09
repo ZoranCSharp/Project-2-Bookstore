418d9f4c0a058302b0363219b16fda16374bddbc
ZoranKJava <zorank.java@gmail.com>
2018-10-09 02:05:01 +0100

    AdminPortal - Add Login Function

--- Bookstore/src/main/java/com/bookstore/repository/UserRepository.java
+++ Bookstore/src/main/java/com/bookstore/repository/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import com.bookstore.domain.User;
 
 public interface UserRepository extends CrudRepository<User, Long> {
+	
 	User findByUsername(String username);
 	
 	User findByEmail(String email);
