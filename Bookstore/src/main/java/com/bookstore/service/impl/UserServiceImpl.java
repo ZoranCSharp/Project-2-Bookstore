a7aa5e678930c0cbed22095e9b9b8d30f737bcef
ZoranKJava <zorank.java@gmail.com>
2018-10-22 13:43:33 +0100

    typo's

--- Bookstore/src/main/java/com/bookstore/service/impl/UserServiceImpl.java
+++ Bookstore/src/main/java/com/bookstore/service/impl/UserServiceImpl.java
@@ -48,7 +48,7 @@ public class UserServiceImpl implements UserService{
 	}
 	
 	@Override
-	public User findByEmail (String email) {
+	public User findByEmail(String email) {
 		return userRepository.findByEmail(email);
 	}
 	
