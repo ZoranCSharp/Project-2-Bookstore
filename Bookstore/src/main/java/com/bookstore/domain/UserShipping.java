0b42dc501f554df781565395c4714307922aa354
ZoranKJava <zorank.java@gmail.com>
2018-10-23 11:55:31 +0100

    Bookstore - Add New Shipping Logic - UserShipping

--- Bookstore/src/main/java/com/bookstore/domain/UserShipping.java
+++ Bookstore/src/main/java/com/bookstore/domain/UserShipping.java
@@ -19,7 +19,8 @@ public class UserShipping {
 	private String userShippingCity;
 	private String userShippingState;
 	private String userShippingCountry;
-	private String userShippingZipCode;
+	private String userShippingZipcode;
+	private boolean userShippingDefault;
 	
 	@ManyToOne
 	@JoinColumn(name="user_id")
@@ -81,12 +82,12 @@ public class UserShipping {
 		this.userShippingCountry = userShippingCountry;
 	}
 
-	public String getUserShippingZipCode() {
-		return userShippingZipCode;
+	public String getUserShippingZipcode() {
+		return userShippingZipcode;
 	}
 
-	public void setUserShippingZipCode(String userShippingZipCode) {
-		this.userShippingZipCode = userShippingZipCode;
+	public void setUserShippingZipcode(String userShippingZipcode) {
+		this.userShippingZipcode = userShippingZipcode;
 	}
 
 	public User getUser() {
@@ -95,6 +96,14 @@ public class UserShipping {
 
 	public void setUser(User user) {
 		this.user = user;
+	}
+
+	public boolean isUserShippingDefault() {
+		return userShippingDefault;
+	}
+
+	public void setUserShippingDefault(boolean userShippingDefault) {
+		this.userShippingDefault = userShippingDefault;
 	}
 	
 	
