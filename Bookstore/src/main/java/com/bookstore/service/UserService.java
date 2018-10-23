4db3e326ca68f1be160beb872ced19854216b49b
ZoranKJava <zorank.java@gmail.com>
2018-10-23 11:56:04 +0100

    Bookstore - Add New Shipping Logic - UserService

--- Bookstore/src/main/java/com/bookstore/service/UserService.java
+++ Bookstore/src/main/java/com/bookstore/service/UserService.java
@@ -5,6 +5,7 @@ import java.util.Set;
 import com.bookstore.domain.User;
 import com.bookstore.domain.UserBilling;
 import com.bookstore.domain.UserPayment;
+import com.bookstore.domain.UserShipping;
 import com.bookstore.domain.security.PasswordResetToken;
 import com.bookstore.domain.security.UserRole;
 
@@ -21,7 +22,11 @@ public interface UserService {
 	
 	User save(User user);
 	
+	
 	void updateUserBilling(UserBilling userBilling, UserPayment userPayment, User user);
 	
 	void setUserDefaultPayment(Long userPaymentId, User user);
+	
+	
+	void updateUserShipping(UserShipping userShipping, User user);
 }
