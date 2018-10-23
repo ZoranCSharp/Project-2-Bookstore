66cd9f518c80c957b69d67b5e32a2704eb6bcf9e
ZoranKJava <zorank.java@gmail.com>
2018-10-23 11:55:56 +0100

    Bookstore - Add New Shipping Logic - UserServiceImpl

--- Bookstore/src/main/java/com/bookstore/service/impl/UserServiceImpl.java
+++ Bookstore/src/main/java/com/bookstore/service/impl/UserServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import com.bookstore.domain.User;
 import com.bookstore.domain.UserBilling;
 import com.bookstore.domain.UserPayment;
+import com.bookstore.domain.UserShipping;
 import com.bookstore.domain.security.PasswordResetToken;
 import com.bookstore.domain.security.UserRole;
 import com.bookstore.repository.PasswordResetTokenRepository;
@@ -115,6 +116,15 @@ public class UserServiceImpl implements UserService{
 		}
 		
 		
+	}
+
+	@Override
+	public void updateUserShipping(UserShipping userShipping, User user) {
+		
+		userShipping.setUser(user);
+		userShipping.setUserShippingDefault(true);
+		user.getUserShippingList().add(userShipping);
+		save(user);
 	}
 	
 
