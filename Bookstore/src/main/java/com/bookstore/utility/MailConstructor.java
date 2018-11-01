6d956720f8d2a9f33e55c045ce14ad4b5a29f45b
ZoranKJava <zorank.java@gmail.com>
2018-11-01 14:17:40 +0100

    Bookstore - Fix UpdateUserInfo

--- Bookstore/src/main/java/com/bookstore/utility/MailConstructor.java
+++ Bookstore/src/main/java/com/bookstore/utility/MailConstructor.java
@@ -30,7 +30,7 @@ public class MailConstructor {
 			) {
 		
 		String url = contextPath + "/newUser?token="+token;
-		String message = "\nPlease click on this link to verify your email and edit your personal information. Your password is: \n"+password;
+		String message = "\nPlease click on this link to verify your email and edit your personal information. \n\nYour password is: \n"+password;
 		SimpleMailMessage email = new SimpleMailMessage();
 		email.setTo(user.getEmail());
 		email.setSubject("House of Leaves Bookstore - New User");
