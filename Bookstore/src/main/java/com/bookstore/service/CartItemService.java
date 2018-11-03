e83eb9887c118c42a0becec65be899fd8964d165
ZoranKJava <zorank.java@gmail.com>
2018-11-03 10:48:24 +0100

    Bookstore - Add orderDetail Controller - findByOrder method

--- Bookstore/src/main/java/com/bookstore/service/CartItemService.java
+++ Bookstore/src/main/java/com/bookstore/service/CartItemService.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import com.bookstore.domain.Book;
 import com.bookstore.domain.CartItem;
+import com.bookstore.domain.Order;
 import com.bookstore.domain.ShoppingCart;
 import com.bookstore.domain.User;
 
 public interface CartItemService {
+	
 	List<CartItem> findByShoppingCart(ShoppingCart shoppingCart);
 	
 	CartItem updateCartItem(CartItem cartItem);
@@ -20,4 +22,5 @@ public interface CartItemService {
 	
 	CartItem save(CartItem cartItem);
 	
+	List<CartItem> findByOrder(Order order);
 }
